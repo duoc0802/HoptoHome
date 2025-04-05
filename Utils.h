41aa2d0a47d5abff1c54d48c2c045a7fc76e9ff9
Phạm Xuân Được <phamxuanduoc2k6@gmail.com>
2025-04-05 13:02:44 +0700

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -7,8 +7,5 @@
 SDL_Texture* loadTexture(const std::string &path);
 void renderTexture(SDL_Texture* tex, int x, int y, int w = 0, int h = 0);
 SDL_Texture* renderText(const std::string &message, SDL_Color color);
-<<<<<<< HEAD
 
-=======
->>>>>>> a35aef98926204bf1ca75f7cdf7acdc3059513b9
 #endif // UTILS_H
